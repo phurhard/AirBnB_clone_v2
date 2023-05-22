933c90beceed812ce43f70153ebeafe73c19665b
phurhard <phurhardeen@gmail.com>
2023-05-22 05:57:17 +0100

    sample flask

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+if __name__ == "__main__":
+    #python -m web-flask.0-hello_route
+    app.run(host="0.0.0.0", port=5000)
