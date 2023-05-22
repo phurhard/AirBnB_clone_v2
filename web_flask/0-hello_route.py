829a3e1ec0ebee04db819ec234acf37efa35fe03
phurhard <phurhardeen@gmail.com>
2023-05-22 07:50:44 +0100

    requuirements

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
-"""This script is the strt of flask, it 
-starts a flask app and prints hello hbnb to the interface of 
+"""This script is the strt of flask, it
+starts a flask app and prints hello hbnb to the interface of
 the web browser"""
+
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     """This script starts a flask app and prints hello HBNB"""
     return "Hello HBNB!"
+
+
 if __name__ == "__main__":
-    #python -m web-flask.0-hello_route
+    # python -m web-flask.0-hello_route
     app.run(host="0.0.0.0", port=5000)
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""this is an extension of 0-hello hbnb, 
+"""this is an extension of 0-hello hbnb,
 it prints hbnb on a new route"""
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
 def hello():
     """This script returns hello HBNb"""
     return "Hello HBNB!"
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """this scripts returns HBNB when the url is entered"""
     return "HBNB"
+
+
 if __name__ == "__main__":
-    #python -m web-flask.0-hello_route
+    # python -m web-flask.0-hello_route
     app.run(host="0.0.0.0", port=5000)
